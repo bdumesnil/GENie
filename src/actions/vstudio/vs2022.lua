d0d974623225b190dc657d74572a158e312ba6ef
bdumesnil <dumesnil.benoit@gmail.com>
2023-12-06 01:03:01 +0100

    Don't create .vcxproj.user/.csproj.user in VS2022 to not overwrite existing settings.

--- src/actions/vstudio/vs2022.lua
+++ src/actions/vstudio/vs2022.lua
@@ -35,11 +35,11 @@
 		onproject = function(prj)
 			if premake.isdotnetproject(prj) then
 				premake.generate(prj, "%%.csproj", vstudio.cs2005.generate)
-				premake.generate(prj, "%%.csproj.user", vstudio.cs2005.generate_user)
+				-- premake.generate(prj, "%%.csproj.user", vstudio.cs2005.generate_user)
 			else
 				premake.vstudio.needAppxManifest = false
 				premake.generate(prj, "%%.vcxproj", premake.vs2010_vcxproj)
-				premake.generate(prj, "%%.vcxproj.user", premake.vs2010_vcxproj_user)
+				-- premake.generate(prj, "%%.vcxproj.user", premake.vs2010_vcxproj_user)
 				premake.generate(prj, "%%.vcxproj.filters", vstudio.vc2010.generate_filters)
 
 				if premake.vstudio.needAppxManifest then
